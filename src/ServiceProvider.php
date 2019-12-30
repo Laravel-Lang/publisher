76da8b2c0898172dd9ce5b9dddc193dd543e161c
Andrey Helldar <helldar@ai-rus.com>
2019-12-30 11:32:59 +0300

    Removed unused method

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -7,24 +7,8 @@ use Helldar\LaravelLangPublisher\Console\LangUpdate;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
-    /**
-     * Indicates if loading of the provider is deferred.
-     *
-     * @var bool
-     */
     protected $defer = false;
 
-    /**
-     * Register the service provider.
-     */
-    public function register()
-    {
-        //
-    }
-
-    /**
-     * Bootstrap the application events.
-     */
     public function boot()
     {
         $this->commands([
