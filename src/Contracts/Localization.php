d604cbcb3b7e3806e0d0fb870622344157e59948
Andrey Helldar <helldar@ai-rus.com>
2020-03-04 00:02:02 +0300

    Removed doc-block

--- src/Contracts/Localization.php
+++ src/Contracts/Localization.php
@@ -4,12 +4,6 @@ namespace Helldar\LaravelLangPublisher\Contracts;
 
 interface Localization
 {
-    /**
-     * Publish Localization.
-     *
-     * @param string $locale
-     * @param bool $force
-     */
     public function publish(string $locale, bool $force = false): void;
 
     public function getResult(): array;
