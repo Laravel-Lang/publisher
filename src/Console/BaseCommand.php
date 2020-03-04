92a1c412f49fe2c88b2d1a6baac8fa8e5fdb74dc
Andrey Helldar <helldar@ai-rus.com>
2020-03-04 18:11:56 +0300

    Fixed coveraging code

--- src/Console/BaseCommand.php
+++ src/Console/BaseCommand.php
@@ -7,15 +7,21 @@ use Illuminate\Console\Command;
 
 abstract class BaseCommand extends Command
 {
-    protected function showResult(array $values, string $status_if_empty): void
+    protected function showResult(array $values, string $message): void
     {
-        empty($values)
-            ? $this->warn($status_if_empty)
-            : $this->table($this->headers($values), $values);
+        empty($values) ? $this->warn($message) : $this->showTable($values);
     }
 
     protected function headers(array $values): array
     {
         return Arr::keys(Arr::first($values));
+    }
+
+    protected function showTable(array $values): void
+    {
+        $this->table(
+            $this->headers($values),
+            $values
+        );
     }
 }
