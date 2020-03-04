1b5a98c8b8307177f74bde368a9d40b3ff07da81
Andrey Helldar <helldar@ai-rus.com>
2020-03-04 07:43:55 +0000

    Apply fixes from StyleCI

--- tests/Commands/InstallTest.php
+++ tests/Commands/InstallTest.php
@@ -2,12 +2,11 @@
 
 namespace Tests\Commands;
 
+use function compact;
 use Helldar\LaravelLangPublisher\Exceptions\SourceLocaleNotExists;
 use Illuminate\Support\Facades\Lang;
 use Symfony\Component\Console\Exception\RuntimeException;
 use Tests\TestCase;
-
-use function compact;
 
 class InstallTest extends TestCase
 {
