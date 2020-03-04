4ebdd4d6bf31bc5e6404591aea626540940c3234
Andrey Helldar <helldar@ai-rus.com>
2020-03-04 17:47:32 +0300

    Updated... tests...

--- tests/Commands/UninstallTest.php
+++ tests/Commands/UninstallTest.php
@@ -2,11 +2,11 @@
 
 namespace Tests\Commands;
 
-use function compact;
 use Helldar\LaravelLangPublisher\Facades\Path;
-use Illuminate\Support\Facades\File;
 use Symfony\Component\Console\Exception\RuntimeException;
 use Tests\TestCase;
+
+use function compact;
 
 class UninstallTest extends TestCase
 {
@@ -21,30 +21,21 @@ class UninstallTest extends TestCase
     public function testUninstall()
     {
         $locales = ['be', 'da', 'gl', 'is'];
-        $lang    = null;
 
-        try {
-            foreach ($locales as $locale) {
-                $lang = $locale;
-                $path = Path::target($locale);
+        $this->artisan('lang:install', compact('locales'))->assertExitCode(0);
 
-                if (! File::exists($path)) {
-                    File::makeDirectory($path);
-                }
-            }
+        foreach ($locales as $locale) {
+            $this->assertDirectoryExists(
+                Path::target($locale)
+            );
+        }
 
-            $this->artisan('lang:uninstall', compact('locales'))->assertExitCode(0);
+        $this->artisan('lang:uninstall', compact('locales'))->assertExitCode(0);
 
-            foreach ($locales as $locale) {
-                $this->assertDirectoryNotExists(
-                    Path::target($locale)
-                );
-            }
-        } catch (\Exception $exception) {
-            $path  = Path::target($lang);
-            $perms = substr(sprintf('%o', fileperms($path)), -4);
-
-            dd(compact('lang', 'perms', 'path'));
+        foreach ($locales as $locale) {
+            $this->assertDirectoryNotExists(
+                Path::target($locale)
+            );
         }
     }
 }
