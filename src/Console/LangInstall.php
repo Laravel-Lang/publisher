8bf9d3a998f4452e1c3124bbd2ca58f6d6ef0b6e
Andrey Helldar <helldar@ai-rus.com>
2020-07-10 14:54:20 +0300

    Fixed command descriptions

--- src/Console/LangInstall.php
+++ src/Console/LangInstall.php
@@ -7,7 +7,7 @@ use Helldar\LaravelLangPublisher\Facades\Locale;
 final class LangInstall extends BaseCommand
 {
     protected $signature = 'lang:install'
-    . ' {locales* : Comma-separated list of, eg: de,tk,it}'
+    . ' {locales* : Space-separated list of, eg: de tk it}'
     . ' {--f|force : Override exiting files}'
     . ' {--j|json : Install JSON files}';
 
