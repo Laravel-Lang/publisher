41c9f4b78d5ff0fa4bceec04e27fb3f4bdb381b5
Andrey Helldar <helldar@ai-rus.com>
2021-04-14 15:03:49 +0300

    Added doc-blocks

--- src/Support/Config.php
+++ src/Support/Config.php
@@ -15,6 +15,11 @@ final class Config
 
     public const KEY_PUBLIC = 'lang-publisher';
 
+    /**
+     * Getting the path to the sources of the English localization.
+     *
+     * @return string
+     */
     public function basePath(): string
     {
         $this->log('Getting the path to the sources of the English localization...');
@@ -22,6 +27,11 @@ final class Config
         return Illuminate::get(self::KEY_PRIVATE . '.path.base');
     }
 
+    /**
+     * Getting the path to localizations.
+     *
+     * @return string
+     */
     public function localesPath(): string
     {
         $this->log('Getting the path to localizations...');
@@ -29,13 +39,23 @@ final class Config
         return Illuminate::get(self::KEY_PRIVATE . '.path.locales');
     }
 
+    /**
+     * Getting the path to resources of the application.
+     *
+     * @return string
+     */
     public function resourcesPath(): string
     {
-        $this->log('Getting the path to resources...');
+        $this->log('Getting the path to resources of the application...');
 
         return Illuminate::get(self::KEY_PRIVATE . '.path.target');
     }
 
+    /**
+     * Determines what type of files to use when updating language files.
+     *
+     * @return bool
+     */
     public function hasInline(): bool
     {
         $this->log('Determines what type of files to use when updating language files...');
@@ -43,13 +63,23 @@ final class Config
         return Illuminate::get(self::KEY_PUBLIC . '.inline');
     }
 
+    /**
+     * Determines whether values should be aligned when saving.
+     *
+     * @return bool
+     */
     public function hasAlignment(): bool
     {
-        $this->log('Do arrays need to be aligned by keys before processing arrays?');
+        $this->log('Determines whether values should be aligned when saving...');
 
         return Illuminate::get(self::KEY_PUBLIC . '.alignment');
     }
 
+    /**
+     * Key exclusion when combining.
+     *
+     * @return array
+     */
     public function excludes(): array
     {
         $this->log('Key exclusion when combining...');
@@ -57,6 +87,11 @@ final class Config
         return Illuminate::get(self::KEY_PUBLIC . '.exclude', []);
     }
 
+    /**
+     * List of ignored localizations.
+     *
+     * @return array
+     */
     public function ignores(): array
     {
         $this->log('List of ignored localizations...');
@@ -64,6 +99,11 @@ final class Config
         return Illuminate::get(self::KEY_PUBLIC . '.ignore', []);
     }
 
+    /**
+     * Getting the value of the option to change the case of keys.
+     *
+     * @return int
+     */
     public function getCase(): int
     {
         $this->log('Getting the value of the option to change the case of keys...');
@@ -71,16 +111,26 @@ final class Config
         return Illuminate::get(self::KEY_PUBLIC . '.case', Caseable::NO_CASE);
     }
 
+    /**
+     * Getting the default localization name.
+     *
+     * @return string
+     */
     public function defaultLocale(): string
     {
-        $this->log('Obtaining a default localization key...');
+        $this->log('Getting the default localization name...');
 
         return Illuminate::get('app.locale') ?: $this->fallbackLocale();
     }
 
+    /**
+     * Getting the fallback localization name.
+     *
+     * @return string
+     */
     public function fallbackLocale(): string
     {
-        $this->log('Obtaining a fallback localization key...');
+        $this->log('Getting the fallback localization name...');
 
         return Illuminate::get('app.fallback_locale') ?: LocalesList::ENGLISH;
     }
