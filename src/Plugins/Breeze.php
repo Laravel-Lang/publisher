8605528f5600ceb387352fb50511624d006270c4
Andrey Helldar <helldar@ai-rus.com>
2021-05-27 13:17:18 +0300

    Laravel Breeze support

---
+++ src/Plugins/Breeze.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Helldar\LaravelLangPublisher\Plugins;
+
+final class Breeze extends Plugin
+{
+    public function vendor(): string
+    {
+        return 'laravel/breeze';
+    }
+
+    public function source(): array
+    {
+        return ['packages/breeze.json'];
+    }
+}
