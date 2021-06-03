8fe496f80e196e51fad191e70ada368178b0b204
Andrey Helldar <helldar@ai-rus.com>
2021-06-03 19:45:18 +0300

    Updated PluginsTest.php

--- tests/Support/PluginsTest.php
+++ tests/Support/PluginsTest.php
@@ -2,9 +2,12 @@
 
 namespace Tests\Support;
 
+use Helldar\LaravelLangPublisher\Plugins\Breeze;
 use Helldar\LaravelLangPublisher\Plugins\Cashier;
 use Helldar\LaravelLangPublisher\Plugins\Fortify;
+use Helldar\LaravelLangPublisher\Plugins\Framework;
 use Helldar\LaravelLangPublisher\Plugins\Jetstream;
+use Helldar\LaravelLangPublisher\Plugins\Lumen;
 use Helldar\LaravelLangPublisher\Plugins\Nova;
 use Helldar\LaravelLangPublisher\Plugins\SparkPaddle;
 use Helldar\LaravelLangPublisher\Plugins\SparkStripe;
@@ -14,9 +17,12 @@ final class PluginsTest extends TestCase
 {
     public function testInstalled()
     {
+        $this->assertFalse(Breeze::make()->has());
         $this->assertFalse(Cashier::make()->has());
         $this->assertFalse(SparkPaddle::make()->has());
+        $this->assertFalse(Lumen::make()->has());
 
+        $this->assertTrue(Framework::make()->has());
         $this->assertTrue(Fortify::make()->has());
         $this->assertTrue(Jetstream::make()->has());
         $this->assertTrue(Nova::make()->has());
