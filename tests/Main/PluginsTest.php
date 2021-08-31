62ac5792e7a5b081c0bbbef7a42611fca5bb6f52
Andrey Helldar <helldar@ai-rus.com>
2021-08-31 21:48:02 +0300

    Added preset for testing

---
+++ tests/Main/PluginsTest.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the "andrey-helldar/laravel-lang-publisher" project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Andrey Helldar <helldar@ai-rus.com>
+ *
+ * @copyright 2021 Andrey Helldar
+ *
+ * @license MIT
+ *
+ * @see https://github.com/andrey-helldar/laravel-lang-publisher
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Main;
+
+use LaravelLang\Lang\Publisher\Plugins\Laravel;
+use Tests\TestCase;
+
+class PluginsTest extends TestCase
+{
+    public function testHas()
+    {
+        $this->assertTrue((new Laravel())->has());
+    }
+}
