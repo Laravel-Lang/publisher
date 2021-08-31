fc98eb42a818cdc3e22fcd29560b4044a7f01f7b
Andrey Helldar <helldar@ai-rus.com>
2021-08-31 23:10:37 +0300

    Added processor presets

---
+++ src/Processors/Add.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file is part of the "andrey-helldar/laravel-lang-publisher" project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Andrey Helldar <helldar@ai-rus.com>
+ *
+ * @copyright 2021 Andrey Helldar
+ *
+ * @license MIT
+ *
+ * @see https://github.com/andrey-helldar/laravel-lang-publisher
+ */
+
+declare(strict_types=1);
+
+namespace Helldar\LaravelLangPublisher\Processors;
+
+use Helldar\Contracts\LangPublisher\Processor;
+use Helldar\Contracts\LangPublisher\Provider;
+
+class Add extends BaseProcessor
+{
+    public function provider(Provider $provider): Processor
+    {
+        // TODO: Implement provider() method.
+    }
+}
---
+++ src/Processors/BaseProcessor.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+ * This file is part of the "andrey-helldar/laravel-lang-publisher" project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Andrey Helldar <helldar@ai-rus.com>
+ *
+ * @copyright 2021 Andrey Helldar
+ *
+ * @license MIT
+ *
+ * @see https://github.com/andrey-helldar/laravel-lang-publisher
+ */
+
+declare(strict_types=1);
+
+namespace Helldar\LaravelLangPublisher\Processors;
+
+use Helldar\Contracts\LangPublisher\Processor;
+
+abstract class BaseProcessor implements Processor
+{
+    protected $force = false;
+
+    public function hasForce(bool $force = false): Processor
+    {
+        $this->force = $force;
+
+        return $this;
+    }
+}
