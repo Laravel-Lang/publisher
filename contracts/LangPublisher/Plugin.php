983554e915bb2500a304de21abc2b55a20fdce91
Andrey Helldar <helldar@ai-rus.com>
2021-09-23 22:15:18 +0300

    🤗

--- contracts/LangPublisher/Plugin.php
+++ contracts/LangPublisher/Plugin.php
@@ -1,5 +1,20 @@
 <?php
 
+/*
+ * This file is part of the "andrey-helldar/laravel-lang-publisher" project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Andrey Helldar <helldar@ai-rus.com>
+ *
+ * @copyright 2021 Andrey Helldar
+ *
+ * @license MIT
+ *
+ * @see https://github.com/andrey-helldar/laravel-lang-publisher
+ */
+
 declare(strict_types=1);
 
 namespace Helldar\Contracts\LangPublisher;
