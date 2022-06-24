bee8fec3dba306b3ad3664359aa7c59e0a9d8778
Andrey Helldar <helldar@ai-rus.com>
2022-06-24 12:47:57 +0300

    Update copyrights

--- src/Constants/Locales.php
+++ src/Constants/Locales.php
@@ -1,5 +1,20 @@
 <?php
 
+/*
+ * This file is part of the "Laravel-Lang/publisher" project.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author Andrey Helldar <helldar@dragon-code.pro>
+ *
+ * @copyright 2022 Andrey Helldar
+ *
+ * @license MIT
+ *
+ * @see https://github.com/Laravel-Lang/publisher
+ */
+
 declare(strict_types=1);
 
 namespace LaravelLang\Publisher\Constants;
