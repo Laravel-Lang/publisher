1ba19e1531f0ddf404c62f12e90b5e83de939513
mohamedsabil83 <me@mohamedsabil83.com>
2023-11-18 13:43:57 +0300

    Ability to set some configurations from .env file

--- config/public.php
+++ config/public.php
@@ -32,7 +32,7 @@ return [
      * By default, `false`.
      */
 
-    'inline' => false,
+    'inline' => env('LANG_PUBLISHER_INLINE', false),
 
     /*
      * Do arrays need to be aligned by keys before processing arrays?
@@ -40,7 +40,7 @@ return [
      * By default, true
      */
 
-    'align' => true,
+    'align' => env('LANG_PUBLISHER_ALIGN', true),
 
     /*
      * This option determines the mechanism for converting translation
