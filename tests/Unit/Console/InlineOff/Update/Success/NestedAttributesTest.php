e33931a17fd4f907eef8f44af3bc58b83f82db2b
Andrey Helldar <helldar@dragon-code.pro>
2025-03-03 13:28:13 +0300

    Removed unnecessary doc-blocks

--- tests/Unit/Console/InlineOff/Update/Success/NestedAttributesTest.php
+++ tests/Unit/Console/InlineOff/Update/Success/NestedAttributesTest.php
@@ -15,19 +15,6 @@
 
 declare(strict_types=1);
 
-/**
- * This file is part of the "laravel-lang/publisher" project.
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- *
- * @author Andrey Helldar <helldar@dragon-code.pro>
- * @copyright 2025 Laravel Lang Team
- * @license MIT
- *
- * @see https://laravel-lang.com
- */
-
 namespace Tests\Unit\Console\InlineOff\Update\Success;
 
 use DragonCode\Support\Facades\Filesystem\Directory;
