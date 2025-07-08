422c4b596d359bd3b98cfb85132d05bf241d05b0
Christof Verhoeven <christof@carscan.be>
2025-07-08 10:09:10 +0200

    prevent loss of attribute translations with subkeys

--- src/Services/Filesystem/Php.php
+++ src/Services/Filesystem/Php.php
@@ -19,6 +19,7 @@ namespace LaravelLang\Publisher\Services\Filesystem;
 
 use DragonCode\PrettyArray\Services\File;
 use DragonCode\Support\Facades\Helpers\Arr;
+use DragonCode\Support\Facades\Helpers\Str;
 use DragonCode\Support\Helpers\Ables\Arrayable;
 use Illuminate\Support\Arr as IlluminateArr;
 
@@ -39,10 +40,36 @@ class Php extends Base
     {
         $content = $this->sort($content);
 
+        // Gathering attribute keys to prevent loss of translations after expand/flatten process.
+        $flatAttributes = [];
+        foreach ($content as $key => $value) {
+            if (Str::startsWith($key, "attributes.")) {
+                $flatAttributes[preg_replace("/^attributes\./", "", $key)] = $value;
+            }
+        }
+
         $content = $this->expand($content);
 
         if ($this->hasValidation($path)) {
             $content = $this->validationSort($content);
+        }
+
+        if (! empty($flatAttributes)) {
+            foreach ($flatAttributes as $key => $value) {
+                // Keys can be removed during the expand/flatten process if a key has a translation and subkeys.
+                //
+                // eg. ['attributes.vehicle' => 'vehicle translated', 'attributes.vehicle.make' => 'make translated']
+                // after expand: ['attributes' => ['vehicle' => ['make' => 'make translated']]]
+                // ... and the vehicle translation is dropped.
+                //
+                // Checking here if the flat attribute keys we gathered before the expand still exist in the result.
+                // If not we add them back into the attribute array.
+                if (! isset($content['attributes'][$key])) {
+                    $content['attributes'][$key] = $value;
+                }
+            }
+
+            $content['attributes'] = $this->sort($content['attributes']);
         }
 
         $content = $this->format($content);
